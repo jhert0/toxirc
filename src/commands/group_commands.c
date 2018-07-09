95c8b61486e30584905ffce8b9969de4456d620a
endoffile78 <endoffile78@yahoo.com>
2018-07-09 16:36:21 -0500

    Disable users and topic commands for groupchats for now

--- src/commands/group_commands.c
+++ src/commands/group_commands.c
@@ -8,19 +8,20 @@
 #include <stdio.h>
 #include <string.h>
 
-static bool command_users(Tox *tox, IRC *irc, uint32_t group_num, char *arg);
+//static bool command_users(Tox *tox, IRC *irc, uint32_t group_num, char *arg);
 static bool command_channel(Tox *tox, IRC *irc, uint32_t group_num, char *arg);
-static bool command_topic(Tox *tox, IRC *irc, uint32_t group_num, char *arg);
+//static bool command_topic(Tox *tox, IRC *irc, uint32_t group_num, char *arg);
 static bool command_help(Tox *tox, IRC *irc, uint32_t group_num, char *arg);
 
 struct Command group_commands[MAX_CMDS] = {
-    { "!user",    "Gets all the users in the linked IRC channel.",                       false, command_users   },
+    //{ "!user",    "Gets all the users in the linked IRC channel.",                       false, command_users   },
     { "!channel", "Gets the channel this groupchat is being synced with.",               false, command_channel },
-    { "!topic",   "Gets the topic for the channel this groupchat is being synced with.", false, command_topic   },
-    { "!help",    "This comamnd",                                                        false, command_help    },
+    //{ "!topic",   "Gets the topic for the channel this groupchat is being synced with.", false, command_topic   },
+    { "!help",    "This command.",                                                       false, command_help    },
     { NULL,       NULL,                                                                  false, NULL            },
 };
 
+#if 0
 static bool command_users(Tox *tox, IRC *irc, uint32_t group_num, char *UNUSED(arg)){
     char *channel_name = irc_get_channel_by_group(irc, group_num);
 
@@ -28,6 +29,7 @@ static bool command_users(Tox *tox, IRC *irc, uint32_t group_num, char *UNUSED(a
 
     return true;
 }
+#endif
 
 static bool command_channel(Tox *tox, IRC *irc, uint32_t group_num, char *UNUSED(arg)){
     char *channel_name = irc_get_channel_by_group(irc, group_num);
@@ -40,9 +42,15 @@ static bool command_channel(Tox *tox, IRC *irc, uint32_t group_num, char *UNUSED
     return true;
 }
 
+#if 0
 static bool command_topic(Tox *tox, IRC *irc, uint32_t group_num, char *UNUSED(arg)){
+    char *channel_name = irc_get_channel_by_group(irc, group_num);
+
+    irc_command_topic(irc, channel_name, NULL);
+
     return true;
 }
+#endif
 
 static bool command_help(Tox *tox, IRC *UNUSED(irc), uint32_t group_num, char *UNUSED(arg)){
     for (int i = 0; group_commands[i].cmd; i++) {
